a9bef575f5f8606ba3108e41e6ce2583da2a6298
aswin <aswin0484@gmail.com>
2023-09-11 15:13:08 +0530

    Initial Commit

---
+++ whatsappclone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  whatsappclone
+//
+//  Created by ASWIN A on 11/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
